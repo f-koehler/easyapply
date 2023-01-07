bc61a8c0768469558af310c34f8b684ea1f3efa5
Fabian Köhler <fabian.koehler@protonmail.ch>
2023-01-07 22:28:38 +0100

    Render using playwright, for now not headless.

--- easyapply/pdf/__init__.py
+++ easyapply/pdf/__init__.py
@@ -1,31 +1,18 @@
 import shutil
-import subprocess
 from pathlib import Path
-import logging
-from functools import cache
-
-LOGGER = logging.getLogger(__name__)
-
-
-@cache
-def find_chromium() -> Path:
-    if path := shutil.which("chromium"):
-        LOGGER.info("Found chromium at %s", str(path))
-        return Path(path)
-
-    raise RuntimeError("Chromium not found")
+from playwright.sync_api import sync_playwright
 
 
 def render_file(path: Path, output: Path, delay: int = 100) -> None:
-    cmd = [
-        str(find_chromium()),
-        "--headless",
-        "--disable-gpu",
-        "--run-all-compositor-stages-before-draw",
-        f"--virtual-time-budget={delay}",
-        "--print-to-pdf-no-header",
-        f"--print-to-pdf={output}",
-        f"file://{path.resolve()}",
-    ]
-    LOGGER.info("Running command: %s", " ".join(cmd))
-    subprocess.check_output(cmd)
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto(f"file://{path.resolve()}")
+        page.pdf(
+            path=output,
+            print_background=True,
+            display_header_footer=False,
+            prefer_css_page_size=True,
+        )
+        page.wait_for_load_state("networkidle")
+        browser.close()
