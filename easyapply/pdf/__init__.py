840c3416454ab2ab8f1e5e27aedc7a6d6257d446
Fabian Köhler <fabian.koehler@protonmail.ch>
2023-01-07 22:39:48 +0100

    Fix PDF rendering process and make it headless.

--- easyapply/pdf/__init__.py
+++ easyapply/pdf/__init__.py
@@ -1,18 +1,19 @@
 import shutil
 from pathlib import Path
 from playwright.sync_api import sync_playwright
+import time
 
 
 def render_file(path: Path, output: Path, delay: int = 100) -> None:
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
         page.goto(f"file://{path.resolve()}")
+        page.wait_for_load_state("networkidle")
         page.pdf(
             path=output,
             print_background=True,
             display_header_footer=False,
             prefer_css_page_size=True,
         )
-        page.wait_for_load_state("networkidle")
         browser.close()
