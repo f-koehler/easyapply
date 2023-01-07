d1066a57f02059f6473a2f2298572bd872f6e42f
Fabian Köhler <fabian.koehler@protonmail.ch>
2023-01-07 23:47:20 +0100

    Improve CLI for build command.

--- easyapply/cli/__init__.py
+++ easyapply/cli/__init__.py
@@ -4,6 +4,8 @@ import typer
 import yaml
 import logging
 import os
+import tempfile
+import shutil
 
 from .. import pdf, themes
 
@@ -20,19 +22,39 @@ def list_themes():
 
 
 @app.command()
-def build():
-    with open("application.yaml") as fptr:
-        config = yaml.safe_load(fptr)
-    template = themes.load_template(config["theme"]["name"])
-    (Path.cwd() / "cv.html").write_text(
-        template.render(
-            theme=config["theme"],
-            cv=config["cv"],
-            theme_dir=Path(template.filename).parent.parent,
-        ),
-    )
+def build(
+    output: Path = Path.cwd() / "cv.pdf",
+    input: Path = Path.cwd() / "application.yaml",
+):
+    input = input.resolve()
+    output = output.resolve()
 
-    pdf.render_file(Path.cwd() / "cv.html", Path.cwd() / "cv.pdf")
+    with tempfile.TemporaryDirectory() as tmpdir:
+        tmppath = Path(tmpdir)
+
+        with open(input, "r") as fptr:
+            config = yaml.safe_load(fptr)
+
+        template = themes.load_template(config["theme"]["name"])
+
+        html_path = tmppath / "cv.html"
+        html_path.write_text(
+            template.render(
+                theme=config["theme"],
+                cv=config["cv"],
+                theme_dir=Path(template.filename).parent.parent,
+            ),
+        )
+
+        match output.suffix:
+            case ".html":
+                shutil.copy2(html_path, output)
+            case ".pdf":
+                pdf_path = tmppath / "cv.pdf"
+                pdf.render_file(html_path, pdf_path)
+                shutil.copy2(pdf_path, output)
+            case _:
+                raise ValueError(f"Unknown output format: {output.suffix}")
 
 
 def main():
