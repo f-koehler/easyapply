d00f6db8e899a5c29336590dd11cf31bbeda7aa8
Fabian Köhler <fabian.koehler@protonmail.ch>
2023-01-11 02:46:30 +0100

    Add CLI command to render a specific template.

--- easyapply/cli/__init__.py
+++ easyapply/cli/__init__.py
@@ -3,6 +3,7 @@ import os
 import shutil
 import tempfile
 from pathlib import Path
+from typing import Any
 
 import typer
 import yaml
@@ -17,25 +18,68 @@ def init_logging():
     logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO").upper())
 
 
-@app.command()
-def list_themes():
-    print("List of themes")
-
-
 RESERVED_KEYS = [
     "theme_dir",
     "build_pdf",
 ]
 
 
+def check_config(config: dict[str, Any]):
+    for reserved in RESERVED_KEYS:
+        if reserved in config:
+            raise ValueError("Reserved key in config: {reserved}")
+
+    if "theme" not in config:
+        raise ValueError("Missing theme setting config")
+
+    if "name" not in config["theme"]:
+        raise ValueError("Missing name setting in confg['theme']")
+
+
+@app.command()
+def render(
+    name: Path = typer.Argument(
+        ...,
+        help="Relative of the template within the theme.",
+    ),
+    input: Path = typer.Option(
+        Path("application.yaml"),
+        "--input",
+        "-i",
+        help="Path to the input YAML file.",
+    ),
+):
+    input = input.resolve()
+
+    with open(input, "r") as fptr:
+        config = yaml.safe_load(fptr)
+
+    check_config(config)
+
+    template = themes.load_template(
+        config["theme"]["name"],
+        template=str(name),
+    )
+    rendered = template.render(
+        theme_dir=Path(template.filename).parent.parent,
+        build_pdf=False,
+        **config,
+    )
+    print(rendered)
+
+
 @app.command()
 def build(
     output: Path = typer.Option(
         Path("cv.pdf"),
+        "--output",
+        "-o",
         help="Path to the output file.",
     ),
     input: Path = typer.Option(
         Path("application.yaml"),
+        "--input",
+        "-i",
         help="Path to the input YAML file.",
     ),
     debug: bool = typer.Option(
@@ -49,12 +93,10 @@ def build(
     with tempfile.TemporaryDirectory() as tmpdir:
         tmppath = Path(tmpdir)
 
-        with open(input) as fptr:
+        with open(input, "r") as fptr:
             config = yaml.safe_load(fptr)
 
-        for reserved in RESERVED_KEYS:
-            if reserved in config:
-                raise ValueError("Reserved key in config: {reserved}")
+        check_config(config)
 
         template = themes.load_template(config["theme"]["name"])
 
